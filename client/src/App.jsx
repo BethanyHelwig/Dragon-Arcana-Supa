7aef668e194210cbbfddf79ea5f42966ca7f7f3b
PhoenixVerus <73077264+PhoenixVerus@users.noreply.github.com>
2025-11-09 17:51:55 -0600

    Setting up the backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,21 @@ import { useState } from 'react'
 
 function App() {
 
-  return (
-    <>
-      <p>hi</p>
-    </>
-  )
+    const [ data, setData ] = useState(null)
+
+    function getData(){
+        fetch('http://127.0.0.1:8080/api/data')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }
+
+    getData()
+
+    return (
+        <>
+            <p>hi</p>
+        </>
+    )
 }
 
 export default App
--- server/index.js
+++ server/index.js
@@ -1,4 +1,44 @@
-const express = require("express")
-const dotenv = require("dotenv")
+import express from 'express'
+import {} from 'dotenv/config'
+import cors from 'cors'
+import { createClient } from "@supabase/supabase-js"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
+const PORT = process.env.PORT || 8000
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_PUBLISHABLE_KEY)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(cors())
+
+app.get('/api/data', async (req, res) => {
+    try {
+        res.json(await getData())
+    }
+    catch {
+        res.send(err)
+    }
+})
+
+// test function to get data from supabase DB
+async function getData(){
+    // const { data, error } = await supabase.from('skill').select()
+
+    const { data, error } = await supabase
+        .from('ability_score')
+        .select(
+            `
+            *,
+            skill(name, description)
+            `,
+        )
+        .order('full_name')
+    return data
+}
+
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
+    .on('error', (err) => {
+        console.error('Failed to start server: ', err)
+    })
